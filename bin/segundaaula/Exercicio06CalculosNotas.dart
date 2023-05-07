6e0817bcdefaa21d7eda031fc22e3b5f9ef9b888
jbwinchesterjw <49697170+jbwinchesterjw@users.noreply.github.com>
2023-05-07 12:50:09 -0300

    Ré organizado comentários.

--- bin/segundaaula/Exercicio06CalculosNotas.dart
+++ bin/segundaaula/Exercicio06CalculosNotas.dart
@@ -1,10 +1,10 @@
 import 'package:test/expect.dart';
 
-/**
- * Vamos criar um sistema de cálculo de notas? A aplicação deve armazenar uma quantidade N de notas e
-    calcular a média a qual o aluno chegou. A nota para passar de ano é 7.
- */
 void main(List<String> arguments) {
+  /**
+   * Vamos criar um sistema de cálculo de notas? A aplicação deve armazenar uma quantidade N de notas e
+   * calcular a média a qual o aluno chegou. A nota para passar de ano é 7.
+   */
   List<double> notas = [10.0, 9.0, 8.0, 6.5];
   double media = 0.0;
   for (int i = 0; i < notas.length; i++) {
--- bin/segundaaula/Exercicio07ListaNumerosAleatorios.dart
+++ bin/segundaaula/Exercicio07ListaNumerosAleatorios.dart
@@ -1,11 +1,11 @@
 import 'dart:math';
 
-/**
- * Crie uma lista de valores inteiros. Essa lista deve ser populada utilizando somente números aleatórios.
-    Após popular essa lista, remova todos os itens pares dessa lista, deixando somente os valores impares.
-    Em uma nova lista, devem ficar somente os valores pares.
- */
 void main(List<String> arguments) {
+  /**
+   * Crie uma lista de valores inteiros. Essa lista deve ser populada utilizando somente números aleatórios.
+   * Após popular essa lista, remova todos os itens pares dessa lista, deixando somente os valores impares.
+   * Em uma nova lista, devem ficar somente os valores pares.
+   */
   List<int> numerosAleatorios = List.generate(10, (_) => Random().nextInt(10));
   List<int> numerosIpa = [0];
   List<int> numerosPares = [0];
